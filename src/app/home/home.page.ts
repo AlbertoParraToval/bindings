3ee5ff16242dbdcd5cabee514c22c86590742582
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-09-30 10:39:12 +0200

    Actualizar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,35 @@
 import { Component } from '@angular/core';
-import { Person } from "./person/person.component";
+import { Person } from './person/person.component';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
-  name: string = 'Alfonso';
-  age: number;
-  constructor() {}
-  id: number = 0;
-  people: Person[] = [];
 
-  addPerson(n: string) {
+  name:string = "Alberto";
+  people:Person[]=[];
+  id:number=0;
+  
+  constructor() {}
+
+  addPerson(n:string){
     this.people.push({
-      id: this.id++,
-      name: n,
-      age: this.age,
+      id: this.id++, name: n,
     });
+    
   }
 
-  removePerson(id: number) {
-    this.people = this.people.filter(p => p.id != id); //Con esto filtramos que id se borra y los demas se dejan
-    var index = this.people.findIndex((p) => {
-      return p.id == id;
-    });
-    if ((index) >= 0) {
-      this.people.splice(index, 1);
+  removePerson(_id:number){
+    
+    var index = this.people.findIndex((p => p.id == _id)); //vover a preguntar
+
+    if(index >= 0){
+      this.people.splice(index,1);
     }
   }
+
 }
